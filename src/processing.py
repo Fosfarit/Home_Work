a2151feed6068c0df469d5b93262cead971eae03
Alexandr Egoshin <fosfarit45@gmail.com>
2025-09-15 00:57:54 +0500

    fix

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(transactions: list[dict], state: str ='EXECUTED') -> list[dict]:
+def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Функция принимает список словарей
     и опционально значение для ключа state (по умолчанию 'EXECUTED').
@@ -7,9 +7,10 @@ def filter_by_state(transactions: list[dict], state: str ='EXECUTED') -> list[di
     """
     filter_transtaction = []
     for transaction in transactions:
-        if transaction.get('state') == state:
+        if transaction.get("state") == state:
             filter_transtaction.append(transaction)
     return filter_transtaction
+
 
 def sort_by_date(transactions: list[dict], ascending: bool = True) -> list[dict]:
     """
@@ -19,5 +20,5 @@ def sort_by_date(transactions: list[dict], ascending: bool = True) -> list[dict]
     Функция должна возвращать новый список,
     отсортированный по дате.
     """
-    sorted_transactions_date = sorted(transactions, key = lambda x: x['date'], reverse = ascending)
+    sorted_transactions_date = sorted(transactions, key=lambda x: x["date"], reverse=ascending)
     return sorted_transactions_date
